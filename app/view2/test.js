ff055b7c8083b2c1a813d8bbd0e9e29882f4fa29
Highflex69 <roland_5_26@hotmail.com>
2016-04-17 20:16:33 +0200

    projectfile
    simple code to send post-request and put the response into a HTML page

---
+++ app/view2/test.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Teddy on 2016-04-15.
+ */
+var app = angular.module('app', [], function($httpProvider){
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+    $httpProvider.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+});
+
+app.controller("WebApiCtrl", function($scope, $http){
+    $http({
+        url : 'https://api.helldiversgame.com/0.3/',
+        method : "POST",
+        data : 'action=get_campaign_status',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    }).then(function(response) {
+        console.log(response);
+        $scope.result = response;
+    }, function(response) {
+        //fail case
+        console.log(response);
+        $scope.result = response;
+    });
+});
+
+
+
