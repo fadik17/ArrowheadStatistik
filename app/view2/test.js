1950f78fa3cf09c7b833da46a05a437ee0b2e593
danidaryaweesh <dani_kurkees@hotmail.com>
2016-04-22 12:35:32 +0200

    update
    test.js and test.html

--- app/view2/test.js
+++ app/view2/test.js
@@ -1,6 +1,8 @@
 /**
  * Created by Teddy on 2016-04-15.
  */
+
+
 var app = angular.module('app', [], function($httpProvider){
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
     $httpProvider.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
@@ -23,6 +25,59 @@ app.controller("WebApiCtrl", function($scope, $http){
         $scope.result = response.data;
     });
 });
+
+app.controller("TimeLineGraphic", function($scope){
+        $scope.dimension=function(){
+            document.write("Hello!");
+            jQuery(document).ready(function ($) {
+
+                $('#checkbox').change(function(){
+                    setInterval(function () {
+                        moveRight();
+                    }, 3000);
+                });
+
+                var slideCount = $('#slider ul li').length;
+                var slideWidth = $('#slider ul li').width();
+                var slideHeight = $('#slider ul li').height();
+                var sliderUlWidth = slideCount * slideWidth;
+
+                $('#slider').css({ width: slideWidth, height: slideHeight });
+
+                $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+
+                $('#slider ul li:last-child').prependTo('#slider ul');
+
+                function moveLeft() {
+                    $('#slider ul').animate({
+                        left: + slideWidth
+                    }, 200, function () {
+                        $('#slider ul li:last-child').prependTo('#slider ul');
+                        $('#slider ul').css('left', '');
+                    });
+                };
+
+                function moveRight() {
+                    $('#slider ul').animate({
+                        left: - slideWidth
+                    }, 200, function () {
+                        $('#slider ul li:first-child').appendTo('#slider ul');
+                        $('#slider ul').css('left', '');
+                    });
+                };
+
+                $('a.control_prev').click(function () {
+                    moveLeft();
+                });
+
+                $('a.control_next').click(function () {
+                    moveRight();
+                });
+
+            });
+        }
+   
+}
 
 
 
