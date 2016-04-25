a026d3fb1ae758c2f85e42de7e15d41a7e3949d5
Highflex69 <roland_5_26@hotmail.com>
2016-04-25 10:37:19 +0200

    mellan_server
    client can now do a get request to this server and get data from
    campaign.

---
+++ Mellan_Server/src/main/java/com/rest/RestfulServicesAhApplication.java
@@ -0,0 +1,17 @@
+package com.rest;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulServicesAhApplication {
+
+
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(RestfulServicesAhApplication.class, args);
+	}
+}
