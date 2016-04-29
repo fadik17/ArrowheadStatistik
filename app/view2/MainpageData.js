3da353ba25eeccdc4212d692b56f327cf54d3241
danidaryaweesh <dani_kurkees@hotmail.com>
2016-04-29 13:07:25 +0200

    update
    test.js and test.html

--- app/view2/MainpageData.js
+++ app/view2/MainpageData.js
@@ -1,17 +1,14 @@
 var sliderVal=1;
+var enemyType="bugs";
 
 
 function evalSlider(){
-
     sliderVal=document.getElementById('rating').value;
     document.getElementById('sliderValue').innerHTML=sliderVal;
 }
 
-// används inte förnärvarande
-function correctSlider(){
-
-    var sliderVal=document.getElementById('getNumbers').value;
-    document.getElementById('rating').innerHTML=sliderVal;
+function saveEnemyType(){
+    enemyType=document.getElementById('enemyType').value;
 }
 
 
@@ -25,41 +22,33 @@ var app = angular.module('app', [], function($httpProvider){
 app.controller("WebApiCtrl", function($scope, $http){
 
     $http.get("http://localhost:8080/post").then(function(response) {
-
         console.log(response);
         $scope.result = response.data;
     }, function(response) {
 
         //fail case
-        document.write("fail");
+    //    document.write("fail");
         console.log(response);
         $scope.result = response.data;
     });
 
-    $scope.getSnapshot = function() {
-
-        $http({
-            url: "http://localhost:8080/x",
-            method: "GET",
-            params: {"sliderValue": sliderVal}
-        }).success(function(snapResponse) {
-
-            console.log("Success", snapResponse);
-            $scope.snapResult = snapResponse.data;
-        }).error(function() {
-
+    $scope.getSnapshot=function(){
+        $http.get("http://localhost:8080/x",{
+            params:{"SliderValue":sliderVal}
+        }).success(function (snapResponse){
+            console.log("Success",snapResponse);
+            $scope.snapResult=snapResponse.data;
+        }).error(function (){
             console.log("error");
         });
     };
 
     $scope.defaultSlide=function() {
-
         document.getElementById("sliderValue").value = "1";
     }
 
     // möjliggöra dynamisk ändring --> kommer att användas senare
     $scope.getEventSize=function () {
-
         return 50;
     }
 });
