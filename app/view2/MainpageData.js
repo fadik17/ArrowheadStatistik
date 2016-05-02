9c5e57d1d0c9512428aaf674b4c170f09b2b3f66
Fadi <diango@live.se>
2016-05-02 10:59:33 +0200

    Correction

--- app/view2/MainpageData.js
+++ app/view2/MainpageData.js
@@ -1,54 +1,41 @@
 var sliderVal = 1;
 var enemyType = "bugs";
 
-
 function evalSlider() {
+
     sliderVal = document.getElementById('rating').value;
     document.getElementById('sliderValue').innerHTML = sliderVal;
 }
 
 function saveEnemyType() {
+
     enemyType = document.getElementById('enemyType').value;
 }
 
-
 var app = angular.module('app', [], function ($httpProvider) {
+
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
     $httpProvider.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 });
 
-
-app.controller("WebApiCtrl", function ($scope, $http) {
-
-    $http.get("http://localhost:8080/post").then(function (response) {
-        console.log(response);
-        $scope.result = response.data;
-    }, function (response) {
-
-        //fail case
-        //    document.write("fail");
-        console.log(response);
-        $scope.result = response.data;
-    });
-
-    $scope.defaultSlide = function () {
-        document.getElementById("sliderValue").innerHTML = 1;
-    }
-
-    // möjliggöra dynamisk ändring --> kommer att användas senare
-    $scope.getEventSize = function () {
-        return 50;
-    }
-}, function ($scope, $http) {
-    var getGet = function () {
-        $http.get("http://localhost:8080/c=3", {
-            params: {"season": 14, "start": 20, "end": 20}
-        }).success(function (snapResponse) {
-            console.log("Success", snapResponse);
-            $scope.snapResult = snapResponse;
-            getGet();
-        }).error(function () {
-            console.log("error");
+app.service('dataService', function($http) {
+    
+    this.getData = function() {
+        // $http() returns a $promise that we can add handlers with .then()
+        return $http({
+            method: 'GET',
+            url: "http://localhost:8080/c=3",
+            params: {"season": 10, "start": sliderVal, "end": sliderVal}
         });
     }
+});
+
+app.controller("WebApiCtrl", function($scope, dataService) {
+
+    $scope.data = null;
+
+    dataService.getData().then(function(dataResponse) {
+
+        $scope.data = dataResponse;
+    });
 });
